6e43dac9a3fa375044c957c02e0f7e9df954d07d
Keegan Rice <krice@tradeking.com>
2014-10-16 10:34:32 -0400

    default tinker extension

---
+++ event.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	var p = $(".ev-match div:contains('Tinker') .ev-timer").first().text()
+	var n = new Date();
+	var h = p.split(":");
+
+	var soon = (h[0]-n.getHours())* 3600000 + (h[1]-n.getMinutes())*60000;
+	if (soon > 0) {
+		alert('You will be alerted in '+parseInt(soon/1000/60/60,10)+'h '+parseInt(soon/1000/60%60,10)+'m');
+		setTimeout(function () {alert('Watch Dota!')}, soon);
+		return false;
+	}
+});
